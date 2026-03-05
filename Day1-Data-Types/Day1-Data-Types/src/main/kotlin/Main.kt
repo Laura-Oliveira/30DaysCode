4ce4c77f6e47461e0f1ac173f4efa6b8e5e51744
Laura-Oliveira <laura.regina.cp@gmail.com>
2026-03-05 17:07:16 -0300

    30 Days of Code by HackerRank — Day 1: Data Types

---
+++ Day1-Data-Types/Day1-Data-Types/src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+package org.days.code
+
+fun main()
+{
+    var inputInt: Int? = readlnOrNull()?.toIntOrNull()
+    var inputDouble: Double? = readlnOrNull()?.toDoubleOrNull()
+    var inputString: String = readlnOrNull().orEmpty()
+
+    combineData(inputInt, inputDouble, inputString)
+}
+
+//Validate Null or Blank Inputs. Requires a Valid Data Input
+private fun validateFields(inputInt: Int?, inputDouble: Double?, inputString: String): Boolean
+{ return (inputInt != null && inputDouble != null && inputString.isNotBlank()) }
+
+fun combineData(inputInt: Int?, inputDouble: Double?, inputString: String)
+{
+    val dataInt: Int = 4
+    val dataDouble: Double = 4.0
+    val dataString: String = "HackerRank "
+    val isValid = validateFields(inputInt, inputDouble, inputString)
+
+    if (!isValid)
+    { println("Fields cannot be null or empty") }
+    else
+    {
+        val outputInt: Int = dataInt + inputInt!!
+        val outputDouble: Double = dataDouble + inputDouble!!
+        val outputString: String = dataString + inputString
+
+        println(outputInt)
+        println(outputDouble)
+        println(outputString)
+    }
+}
