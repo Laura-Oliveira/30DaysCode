a79f542cc0fcb9a9e63a4b352d0aad1ffab10c6e
Laura <laura@Mac-Puma.local>
2026-03-07 18:59:17 -0300

    30 Days of Code by HackerRank — Day 3: Conditional Statements

---
+++ Day3-Conditional-Statements/src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+package org.days.code
+import kotlin.io.*
+import kotlin.text.*
+
+fun main()
+{
+    val N = readLine()!!.trim().toInt()
+
+    if(N % 2 != 0)
+    {
+        println("Weird")
+    }
+    else
+    {
+        when(N)
+        {
+            in 2..5 -> println("Not Weird")
+            in 6..20 -> println("Weird")
+            else -> println("Not Weird")
+        }
+    }
+}
