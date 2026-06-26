22f6dbe045799fa6c1eb7347f11f076fff51a102
Laura-Oliveira <laura.regina.cp@gmail.com>
2026-06-26 18:01:52 -0300

    [Refactor] Code Refactoring on Day7-Arrays challenge

--- Day7-Arrays/src/main/kotlin/Main.kt
+++ Day7-Arrays/src/main/kotlin/Main.kt
@@ -2,17 +2,23 @@ package org.days.code
 
 fun main()
 {
-    val n = readLine()!!.trim().toInt()
-    val arr = readLine()!!
-        .trimEnd()
-        .split(" ")
-        .map{ it.toInt() }
-        .toTypedArray()
+    val loopCounts = readlnOrNull()?.trim()?.toIntOrNull() ?: 0
 
-    val reversedArray = arr.reversedArray()
-
-    for(count in reversedArray.indices)
+    repeat(loopCounts)
     {
-        print("${reversedArray[count]} ")
+        val inputArray: Array<Int> = readlnOrNull()
+            ?.trim()
+            ?.split(" ")
+            ?.mapNotNull { it.toIntOrNull() }
+            ?.toTypedArray() //returns Array<Int>
+            ?: emptyArray<Int>() //returns a Array<Int> with 0 elements
+
+        //Reverse the Array order, and elements joined with spaces, instead of comma
+        println(inputArray.reversedArray().joinToString(" "))
     }
+
+    //.toIntArray()
+    // returns type List<Int>
+    //.toTypedArray()
+    //returns type Array<Int>
 }
